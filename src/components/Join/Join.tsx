b1a3cddb2a42e90974c35cc477fc2eb81613a324
Ogheneyoma <Lawrenceyoma@gmail.com>
2025-10-18 04:24:14 +0100

    refactor join to use tailwind

--- src/components/Join/Join.tsx
+++ src/components/Join/Join.tsx
@@ -1,18 +1,25 @@
 import Button from "../ui/button/Button"
-import "./Join.scss"
 import Picture from "../../assets/HeroImg.png";
 
 const Join = () => {
     return (
-        <div className="join">
-            <img src={Picture} alt="join-picture" />
-            <div className="joinMain">
-                <div className="joinMainForm">
-                    <h1>Join Our Learning Community</h1>
-                    <p>Subscribe today to stay connected with School activities, academic tips and improving grades.</p>
+        <div className="relative mb-[50px]">
+            <img 
+                src={Picture} 
+                alt="join-picture" 
+                className="hidden lg:block absolute w-[500px] left-[75px] top-[50px]"
+            />
+            <div className="ml-auto w-full lg:w-[75%] bg-[#5d91f7]">
+                <div className="w-[90%] mx-auto lg:ml-auto lg:mr-0 py-10 lg:w-1/2 lg:py-[60px]">
+                    <h1 className="text-[1.5rem] sm:text-[2rem] lg:text-[2.5rem] text-white leading-normal">Join Our Learning Community</h1>
+                    <p className="text-white my-4 w-full lg:w-[80%]">Subscribe today to stay connected with School activities, academic tips and improving grades.</p>
 
-                    <div className="joinMainFormInput">
-                        <input type="text" placeholder="Enter your email address"/>
+                    <div className="my-5">
+                        <input 
+                            type="text" 
+                            placeholder="Enter your email address"
+                            className="w-full lg:w-[80%] rounded-full p-2.5 outline-none"
+                        />
                     </div>
 
                     <Button 
