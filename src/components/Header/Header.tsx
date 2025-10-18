6d1ef69cdbf784b69eff6db24e6a36cfa06e939a
Ogheneyoma <Lawrenceyoma@gmail.com>
2025-10-18 04:37:55 +0100

    chore: fix build errors

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,3 @@
-import './Header.scss';
 import { FacebookIcon, LinkedinIcon } from 'lucide-react';
 import PortalLogo from "../../assets/portalLogo.png";
 import Button from '../ui/button/Button';
@@ -6,12 +5,6 @@ import Button from '../ui/button/Button';
 
 
 const Header = () => {
-    const iconStyles = {
-        padding: "5px", 
-        backgroundColor: "#5d91f7", 
-        color: "white", 
-        borderRadius: "99px"
-    }
     const buttonStyle = {
         padding: "10px 1rem", 
         borderRadius: "20px", 
