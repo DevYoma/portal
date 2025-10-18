965815c9bdb82b8a30cc3bb3dea4e6d24452ab1d
Ogheneyoma <Lawrenceyoma@gmail.com>
2025-10-18 03:02:43 +0100

    feat: Header tailwind complete

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -19,16 +19,27 @@ const Header = () => {
         color: "white"
     }
     return (
-        <div className='header'>
+        <div className="flex items-center justify-between py-5 md:py-5 sm:py-4">
             {/* header left */}
-            <div className="headerLeft">
-                <FacebookIcon size={"2rem"} style={iconStyles}/>
-                <LinkedinIcon size={"2rem"} style={iconStyles}/>
+            <div className="hidden sm:flex sm:gap-1.5 md:gap-2">
+                <FacebookIcon 
+                    size={"2rem"} 
+                    // style={iconStyles}
+                    className="p-[5px] rounded-full bg-[#5d91f7] text-white"
+                />
+                <LinkedinIcon 
+                    size={"2rem"}
+                    className="p-[5px] rounded-full bg-[#5d91f7] text-white"
+                />
             </div>
 
-            <div className="headerMiddle">
-                <img src={PortalLogo} alt="portal-logo" />
-                <h1>PortalHQ Blog</h1>
+            <div className="flex items-center gap-1.5">
+                <img 
+                    src={PortalLogo} 
+                    alt="portal-logo"
+                    className="w-[50px] h-[50px] sm:w-[35px] sm:h-[35px]"
+                />
+                <h1 className="text-[1.25rem] sm:text-[1.5rem] md:text-[2rem] text-[#5d91f7]">PortalHQ Blog</h1>
             </div>
 
             <div className="headerRight">
